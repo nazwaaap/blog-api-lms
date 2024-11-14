97794baccda7f72db0f44c0c36cc1a864a9b2067
nazwaaap <pradittanazwa@gmail.com>
2024-11-14 17:02:14 +0700

    feat: upload image functionality

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -2,17 +2,18 @@ import Post from "../models/post.js";
 
 export const createPost = async (req, res) => {
     const { title, body } = req.body;
+    const image = req.file ? req.file.filename : null;
 
     try {
-        const post = new Post({ title, body })
-        const savedPost = await post.save()
+        const post = new Post({ title, body, image });
+        const savedPost = await post.save();
 
         return res.status(201).json({
             message: "Post created",
             data: savedPost
-        })
+        });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Unable to create post"})
+        console.log(error);
+        return res.status(500).json({ message: "Unable to create post" });
     }
-}
+};
