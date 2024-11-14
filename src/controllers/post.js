724b4dc79f6585bdbc6d07e33f1830afdea2eb1e
nazwaaap <pradittanazwa@gmail.com>
2024-11-14 18:32:41 +0700

    feat: update and delete blog post endpoint

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.js";
+import { uploadRemover } from "../utils/uploadRemover.js";
 
 // Create a new post
 export const createPost = async (req, res) => {
@@ -52,5 +53,48 @@ export const getPostById = async (req, res) => {
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Error retrieving post" });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const postId = req.params.id
+    const { title, body } = req.body
+    const newImage = req.file ? req.file.filename : null 
+
+    try {
+        const post = await Post.findById(postId)
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found"})
+        }
+
+        if (newImage) {
+            uploadRemover(post.image)
+        }
+
+        post.title = title
+        post.body = body
+        post.image = newImage ? newImage : post.image
+
+        const updatePost = await post.save()
+
+        return res.status(200).json({
+            message: "Post updated",
+            data: updatePost
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: "Error updating post"})
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const postId = req.params.id;
+
+    try {
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: "Error deleting post"})
     }
 }
